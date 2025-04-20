4ef7ce325366710c04f2bf7d684d0b1f67f4a7b7
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-04-20 09:25:22 +0530

    Enhance GlobalExceptionHandler to handle AccountNotFound and UserNotFound exceptions

--- backend/src/main/java/com/flixify/backend/util/GlobalExceptionHandler.java
+++ backend/src/main/java/com/flixify/backend/util/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.flixify.backend.util;
 
+import com.flixify.backend.custom_exceptions.AccountNotFound;
+import com.flixify.backend.custom_exceptions.UserNotFound;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,11 +14,28 @@ import com.flixify.backend.enums.ResponseStatusEnum;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+
+    private ResponseEntity NOT_FOUND(Exception e) {
+
+        ResponseDto responseDto = new ResponseDto(ResponseStatusEnum.FAILURE, e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDto);
+    }
+
     @ExceptionHandler(VideoNotExist.class)
     public ResponseEntity<ResponseDto> videoNotExist(VideoNotExist e) {
 
-        ResponseDto responseDto = new ResponseDto(ResponseStatusEnum.FAILURE, e.getMessage());
+        return NOT_FOUND(e);
+    }
 
-        return ResponseEntity.status(404).body(responseDto);
+    @ExceptionHandler(AccountNotFound.class)
+    public ResponseEntity<ResponseDto> accountNotFound(AccountNotFound e) {
+
+        return NOT_FOUND(e);
+    }
+
+    @ExceptionHandler(UserNotFound.class)
+    public ResponseEntity<ResponseDto> userNotFound(UserNotFound e) {
+
+        return NOT_FOUND(e);
     }
 }
