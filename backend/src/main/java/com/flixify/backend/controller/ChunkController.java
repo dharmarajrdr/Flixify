0276c15f507de46b6a68f9cc93295bcfdb9e71ed
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-04-20 12:45:58 +0530

    Add addChunks endpoint to ChunkController for storing video chunks

--- backend/src/main/java/com/flixify/backend/controller/ChunkController.java
+++ backend/src/main/java/com/flixify/backend/controller/ChunkController.java
@@ -1,18 +1,13 @@
 package com.flixify.backend.controller;
 
-import com.flixify.backend.custom_exceptions.PermissionDenied;
-import com.flixify.backend.custom_exceptions.UserNotFound;
-import com.flixify.backend.custom_exceptions.VideoNotExist;
+import com.flixify.backend.dto.request.AddChunkDto;
 import com.flixify.backend.dto.response.ChunkDto;
 import com.flixify.backend.dto.response.ResponseDto;
 import com.flixify.backend.enums.ResponseStatusEnum;
 import com.flixify.backend.service.ChunkService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,10 +21,18 @@ public class ChunkController {
     }
 
     @GetMapping("/videos/{videoId}/chunks")
-    public ResponseEntity<ResponseDto> getAllChunks(@RequestParam Integer userId, @PathVariable Integer videoId) throws UserNotFound, VideoNotExist, PermissionDenied {
+    public ResponseEntity<ResponseDto> getAllChunks(@RequestParam Integer userId, @PathVariable Integer videoId) {
 
         List<ChunkDto> chunks = chunkService.getAllChunks(userId, videoId);
         ResponseDto responseDto = new ResponseDto(ResponseStatusEnum.SUCCESS, "Fetched the chunks of the given video.", chunks, null);
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
+
+    @PostMapping("/videos/{videoId}/chunks")
+    public ResponseEntity<ResponseDto> addChunks(@RequestParam Integer userId, @PathVariable Integer videoId, @RequestBody List<AddChunkDto> addChunkDtoList) {
+
+        List<ChunkDto> chunks = chunkService.addChunks(userId, videoId, addChunkDtoList);
+        ResponseDto responseDto = new ResponseDto(ResponseStatusEnum.SUCCESS, "Stored the given chunks.", chunks, null);
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }
 }
