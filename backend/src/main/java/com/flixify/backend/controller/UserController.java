50f2d6c142db255dd7597f1ce77edcb0cc1b4e1e
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-04-20 11:05:44 +0530

    Refactor UserController and VideoController to use final fields for services and update endpoint mappings for user and video retrieval

--- backend/src/main/java/com/flixify/backend/controller/UserController.java
+++ backend/src/main/java/com/flixify/backend/controller/UserController.java
@@ -4,29 +4,34 @@ import com.flixify.backend.dto.response.ResponseDto;
 import com.flixify.backend.enums.ResponseStatusEnum;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import com.flixify.backend.custom_exceptions.AccountNotFound;
 import com.flixify.backend.dto.request.AddUserDto;
 import com.flixify.backend.model.User;
 import com.flixify.backend.service.UserService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @PostMapping("/user")
-    public ResponseEntity<ResponseDto> addUser(@RequestBody AddUserDto addUserDto) throws AccountNotFound {
+    public ResponseEntity<ResponseDto> addUser(@RequestBody AddUserDto addUserDto) {
 
         User user = userService.addUser(addUserDto);
         ResponseDto responseDto = new ResponseDto(ResponseStatusEnum.SUCCESS, "User created successfully.", user, null);
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<ResponseDto> getUser(@RequestParam Integer userId) {
+
+        User user = userService.findUserById(userId);
+        ResponseDto responseDto = new ResponseDto(ResponseStatusEnum.SUCCESS, "User details fetched successfully.", user, null);
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 }
--- backend/src/main/java/com/flixify/backend/controller/VideoController.java
+++ backend/src/main/java/com/flixify/backend/controller/VideoController.java
@@ -1,6 +1,5 @@
 package com.flixify.backend.controller;
 
-import com.flixify.backend.custom_exceptions.UserNotFound;
 import com.flixify.backend.dto.response.ResponseDto;
 import com.flixify.backend.enums.ResponseStatusEnum;
 import com.flixify.backend.model.Video;
@@ -8,7 +7,7 @@ import com.flixify.backend.service.VideoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,14 +15,14 @@ import java.util.List;
 @RestController
 public class VideoController {
 
-    private VideoService videoService;
+    private final VideoService videoService;
 
     public VideoController(VideoService videoService) {
         this.videoService = videoService;
     }
 
-    @GetMapping("/video/{userId}")
-    public ResponseEntity<ResponseDto> getAllVideos(@PathVariable Integer userId) {
+    @GetMapping("/videos")
+    public ResponseEntity<ResponseDto> getAllVideos(@RequestParam Integer userId) {
 
         List<Video> videos = videoService.getVideosByUserId(userId);
         ResponseDto responseDto = new ResponseDto(ResponseStatusEnum.SUCCESS, "Fetched the videos of the given user.", videos, null);
