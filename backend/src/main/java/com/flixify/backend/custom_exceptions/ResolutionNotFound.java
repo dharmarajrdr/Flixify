91b568844fe6d4961fd9f39ee1e58dfe7f40f8a0
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-04-20 12:44:51 +0530

    Add custom exception for handling non-existent resolution

---
+++ backend/src/main/java/com/flixify/backend/custom_exceptions/ResolutionNotFound.java
@@ -0,0 +1,9 @@
+package com.flixify.backend.custom_exceptions;
+
+public class ResolutionNotFound extends RuntimeException {
+
+    public ResolutionNotFound(Integer pixel) {
+
+        super("Resolution with pixel '" + pixel + "' does not exist.");
+    }
+}
