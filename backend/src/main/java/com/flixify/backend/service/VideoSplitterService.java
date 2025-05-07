aa658982d9bcae66c8ec90b8e2550ec70775c469
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-07 15:03:42 +0530

    Add method to retrieve VideoSplitterRule in VideoSplitterService

--- backend/src/main/java/com/flixify/backend/service/VideoSplitterService.java
+++ backend/src/main/java/com/flixify/backend/service/VideoSplitterService.java
@@ -1,11 +1,14 @@
 package com.flixify.backend.service;
 
 import com.flixify.backend.model.Chunk;
+import com.flixify.backend.model.VideoSplitterRule;
 
 import java.nio.file.Path;
 import java.util.List;
 
 public interface VideoSplitterService {
+
+    public VideoSplitterRule getVideoSplitterRule();
 
     public List<Chunk> splitVideo(Integer videoId, Path videoFilePath);
 }
