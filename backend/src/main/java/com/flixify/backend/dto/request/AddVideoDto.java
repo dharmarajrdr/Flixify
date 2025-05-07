c65b101bdc819e9d49c6e2440efb7e4a69de7b3e
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-07 20:27:23 +0530

    Add uniqueId field to AddVideoDto and implement toVideo method for Video conversion

--- backend/src/main/java/com/flixify/backend/dto/request/AddVideoDto.java
+++ backend/src/main/java/com/flixify/backend/dto/request/AddVideoDto.java
@@ -1,6 +1,9 @@
 package com.flixify.backend.dto.request;
 
+import com.flixify.backend.model.Video;
 import lombok.Data;
+
+import java.util.UUID;
 
 @Data
 public class AddVideoDto {
@@ -12,4 +15,15 @@ public class AddVideoDto {
     private Long size;
 
     private Integer userId;
+
+    private UUID uniqueId;
+
+    public Video toVideo() {
+        Video video = new Video();
+        video.setTitle(title);
+        video.setDuration(duration);
+        video.setSize(size);
+        video.setUniqueId(uniqueId);
+        return video;
+    }
 }
