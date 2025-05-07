2b727b6d2f01eba4f4b0927a7bf6ef71dc6ea1f5
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-07 15:03:26 +0530

    Implement VideoSplitterRule retrieval in video splitter strategies

--- backend/src/main/java/com/flixify/backend/strategy/VideoSplitter/CountBasedVideoSplitter.java
+++ backend/src/main/java/com/flixify/backend/strategy/VideoSplitter/CountBasedVideoSplitter.java
@@ -1,12 +1,26 @@
 package com.flixify.backend.strategy.VideoSplitter;
 
 import com.flixify.backend.model.Chunk;
+import com.flixify.backend.model.VideoSplitterRule;
+import com.flixify.backend.service.VideoSplitterRuleService;
 import com.flixify.backend.service.VideoSplitterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.nio.file.Path;
 import java.util.List;
 
+@Service
 public class CountBasedVideoSplitter implements VideoSplitterService {
+
+    @Autowired
+    private VideoSplitterRuleService videoSplitterRuleService;
+
+    @Override
+    public VideoSplitterRule getVideoSplitterRule() {
+
+        return videoSplitterRuleService.getVideoSplitterRule("COUNT_BASED_VIDEO_SPLITTER");
+    }
 
     @Override
     public List<Chunk> splitVideo(Integer videoId, Path videoFilePath) {
--- backend/src/main/java/com/flixify/backend/strategy/VideoSplitter/DurationBasedVideoSplitter.java
+++ backend/src/main/java/com/flixify/backend/strategy/VideoSplitter/DurationBasedVideoSplitter.java
@@ -1,7 +1,10 @@
 package com.flixify.backend.strategy.VideoSplitter;
 
 import com.flixify.backend.model.Chunk;
+import com.flixify.backend.model.VideoSplitterRule;
+import com.flixify.backend.service.VideoSplitterRuleService;
 import com.flixify.backend.service.VideoSplitterService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.nio.file.Path;
@@ -9,6 +12,15 @@ import java.util.List;
 
 @Service
 public class DurationBasedVideoSplitter implements VideoSplitterService {
+
+    @Autowired
+    private VideoSplitterRuleService videoSplitterRuleService;
+
+    @Override
+    public VideoSplitterRule getVideoSplitterRule() {
+
+        return videoSplitterRuleService.getVideoSplitterRule("DURATION_BASED_VIDEO_SPLITTER");
+    }
 
     @Override
     public List<Chunk> splitVideo(Integer videoId, Path videoFilePath) {
