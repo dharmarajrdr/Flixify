bce2f43ea8f283c4aa72ff5d017640806f0ecb72
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-07 15:04:14 +0530

    Refactor VideoSplitterRuleEnum names for clarity and add findByName method in VideoSplitterRuleRepository

--- backend/src/main/java/com/flixify/backend/enums/VideoSplitterRuleEnum.java
+++ backend/src/main/java/com/flixify/backend/enums/VideoSplitterRuleEnum.java
@@ -2,9 +2,9 @@ package com.flixify.backend.enums;
 
 public enum VideoSplitterRuleEnum {
 
-    SPLIT_BY_DURATION(1, "SPLIT_BY_DURATION", "Split by duration"),
-    SPLIT_BY_SIZE(2, "SPLIT_BY_SIZE", "Split by size"),
-    SPLIT_BY_CHUNK_COUNT(3, "SPLIT_BY_CHUNK_COUNT", "Split by chunk count");
+    SPLIT_BY_DURATION(1, "DURATION_BASED_VIDEO_SPLITTER", "Split by duration"),
+    SPLIT_BY_SIZE(2, "SIZE_BASED_VIDEO_SPLITTER", "Split by size"),
+    SPLIT_BY_CHUNK_COUNT(3, "COUNT_BASED_VIDEO_SPLITTER", "Split by chunk count");
 
     private final int id;
     private final String name;
--- backend/src/main/java/com/flixify/backend/repository/VideoSplitterRuleRepository.java
+++ backend/src/main/java/com/flixify/backend/repository/VideoSplitterRuleRepository.java
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Repository;
 
 import com.flixify.backend.model.VideoSplitterRule;
 
+import java.util.Optional;
+
 @Repository
 public interface VideoSplitterRuleRepository extends JpaRepository<VideoSplitterRule, Integer> {
 
+    public Optional<VideoSplitterRule> findByName(String name);
 }
