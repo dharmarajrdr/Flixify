817a783a3f6f4d8da609f37b238d9534c952f63c
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-10 12:35:10 +0530

    Add manifest storage path configuration and ensure directories are created on initialization

--- backend/src/main/java/com/flixify/backend/config/PathConfig.java
+++ backend/src/main/java/com/flixify/backend/config/PathConfig.java
@@ -1,8 +1,11 @@
 package com.flixify.backend.config;
 
+import com.flixify.backend.util.LocalDisk;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
 
 @Component
 public class PathConfig {
@@ -13,12 +16,21 @@ public class PathConfig {
     @Value("${chunk.storage.directory}")
     private String chunkStorageDirectory;
 
+    @Value("${manifest.storage.directory}")
+    private String manifestStoragePath;
+
     public static String VIDEO_STORAGE_DIRECTORY;
     public static String CHUNK_STORAGE_DIRECTORY;
+    public static String MANIFEST_STORAGE_PATH;
 
     @PostConstruct
     public void init() {
         VIDEO_STORAGE_DIRECTORY = videoStorageDirectory;
         CHUNK_STORAGE_DIRECTORY = chunkStorageDirectory;
+        MANIFEST_STORAGE_PATH = manifestStoragePath;
+
+        LocalDisk.createDirectoryIfNotExists(new File(VIDEO_STORAGE_DIRECTORY));
+        LocalDisk.createDirectoryIfNotExists(new File(CHUNK_STORAGE_DIRECTORY));
+        LocalDisk.createDirectoryIfNotExists(new File(MANIFEST_STORAGE_PATH));
     }
 }
