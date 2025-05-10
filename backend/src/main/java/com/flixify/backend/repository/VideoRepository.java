359a726bf744135327b7bab24ac62c4c45c25232
dharmaraj.rd <dharmaraj.rd@zohocorp.com>
2025-05-10 15:29:37 +0530

    Remove redundant method findByIdAndOwner from VideoRepository

--- backend/src/main/java/com/flixify/backend/repository/VideoRepository.java
+++ backend/src/main/java/com/flixify/backend/repository/VideoRepository.java
@@ -15,8 +15,6 @@ public interface VideoRepository extends JpaRepository<Video, Integer> {
 
     public List<Video> findByOwner(User user);
 
-    public Optional<Video> findByIdAndOwner(Integer videoId, User owner);
-
     public Optional<Video> findByFileId(UUID fileId);
 
     public Boolean existsByFileIdAndOwner(UUID fileId, User owner);
